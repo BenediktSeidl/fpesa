86cec90a55d26fdc16a4d822fc3fa3b2fb1ca1af
Benedikt Seidl <Benedikt.Seidl@gmx.de>
2018-01-18 11:09:02 +0100

    frontend: add get message section to dev page

--- frontend/src/components/Dev.vue
+++ frontend/src/components/Dev.vue
@@ -12,6 +12,16 @@
     <h3>multiple</h3>
     <button @click="messagesPostMultiple()">100</button>
     <h2>GET /api/v1/messages/</h2>
+    <h3>request</h3>
+    <form>
+    <label for="paginationId">paginationId</label> <input type="text" id="paginationId" v-model="messagesGetPaginationId"/>
+    <label for="offset">offset</label> <input type="text" id="offset" v-model="messagesGetOffset"/>
+    <label for="limit">limit</label> <input type="text" id="limit" v-model="messagesGetLimit"/>
+    </form>
+    <button @click="messagesGetSingle()">get</button>
+    <h3>response</h3>
+    <pre class="error" v-if="messagesGetError">{{messagesGetError}}</pre>
+    <pre>{{messagesGetResponse}}</pre>
   </div>
 </template>
 
@@ -19,6 +29,23 @@
 export default {
   name: 'Dev',
   methods: {
+    messagesGetSingle (event) {
+      var self = this
+      var params = {
+        'offset': self.messagesGetOffset,
+        'limit': self.messagesGetLimit
+      }
+      if (self.messagesGetPaginationId) {
+        params.paginationId = self.messagesGetPaginationId
+      }
+      self.$http.get('/api/v1/messages/', {params: params}).then(function (response) {
+        this.messagesGetError = ''
+        this.messagesGetResponse = response.body
+      }, function (error) {
+        this.messagesGetResponse = error.body
+        this.messagesGetError = error.body.error.description
+      })
+    },
     messagesPostCheck (event) {
       try {
         JSON.parse(this.messagesPostMessage)
@@ -49,7 +76,12 @@ export default {
   },
   data () {
     return {
-      messagesPostMessage: '{"this": "has", "to": "be", "valid": "json"}',
+      messagesGetOffset: 0,
+      messagesGetError: '',
+      messagesGetLimit: 20,
+      messagesGetPaginationId: '',
+      messagesGetResponse: '',
+      messagesPostMessage: '{"this": "has", "to": "be", "a": "valid", "json": "object"}',
       messagesPostResponse: '',
       messagesPostValid: true
     }
@@ -64,5 +96,22 @@ textarea {
 }
 textarea.invalid {
   background-color: red;
+}
+pre.error {
+  color: #900;
+}
+label {
+  float: left;
+  font-size: 0.8em;
+  clear: both;
+}
+form {
+  width: 100%;
+  float: left;
+  margin-bottom: 0.5em;
+}
+input[type=text]{
+  float: left;
+  clear: both;
 }
 </style>
